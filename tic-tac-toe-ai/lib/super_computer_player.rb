8ebdbfcc45c3aef1308dafa076cd296bc617d05c
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-25 15:58:43 -0400

    Added parts of move method for super_computer_player.rb

--- tic-tac-toe-ai/lib/super_computer_player.rb
+++ tic-tac-toe-ai/lib/super_computer_player.rb
@@ -2,8 +2,41 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    queue = [node]
+    until queue.empty?
+      current = queue.shift
+      return current.prev_move_pos if current.winning_node?(mark)
+
+      node.children.each do |kid|
+        queue << kid
+      end
+      
+    end
+
+    # kids = node.children
+    # kids.each do |child|
+    #   return child.prev_move_pos if child.winning_node?(mark)
+    # end
+
+    kids.select {|child| !child.losing_node?(mark) }.first
+    
   end
 end
+
+# def bfs(target_value)
+#   queue = [self]
+#   until queue.empty?
+#       current = queue.shift
+#       return current if current.value == target_value
+
+#       current.children.each do |child|
+#           queue << child
+#       end
+#   end
+#   nil
+# end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
